7f22eb41bf530a064ffcd5664b6eda8ca968508c
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-11-25 19:24:59 +0530

    add missing config, types and npm scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+  plugins: [react(), tsConfigPaths()],
 });
